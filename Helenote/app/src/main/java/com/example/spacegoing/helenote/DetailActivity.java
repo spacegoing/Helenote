1b1db2653ff7aa0e32ae53339bddf64f2e89b965
spacegoing <spacegoing@gmail.com>
2016-03-23 22:24:08 +1100

    Finished Menu and Activities, Start DB

---
+++ Helenote/app/src/main/java/com/example/spacegoing/helenote/DetailActivity.java
@@ -0,0 +1,29 @@
+package com.example.spacegoing.helenote;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.detail_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+
+}
--- Helenote/app/src/main/java/com/example/spacegoing/helenote/NoteListFragment.java
+++ Helenote/app/src/main/java/com/example/spacegoing/helenote/NoteListFragment.java
@@ -1,6 +1,7 @@
 package com.example.spacegoing.helenote;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -9,6 +10,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -21,6 +23,7 @@ import java.util.List;
  * A simple {@link Fragment} subclass.
  */
 public class NoteListFragment extends Fragment {
+    ArrayAdapter<String> mForecastAdapter;
 
 
     public NoteListFragment() {
@@ -36,7 +39,6 @@ public class NoteListFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        ArrayAdapter<String> mForecastAdapter;
 
         // Create some dummy data for the ListView.  Here's a sample weekly forecast
         String[] data = {
@@ -60,6 +62,15 @@ public class NoteListFragment extends Fragment {
 
         ListView listView = (ListView) rootView.findViewById(R.id.notes_listView);
         listView.setAdapter(mForecastAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String forecast = mForecastAdapter.getItem(position);
+                Intent intent = new Intent(getActivity(), DetailActivity.class)
+                        .putExtra(Intent.EXTRA_TEXT, forecast);
+                startActivity(intent);
+            }
+        });
 
         return rootView;
     }
