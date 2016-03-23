9485ffd7d145d60a9cc9b3c93e6d475fe0bf7704
spacegoing <spacegoing@gmail.com>
2016-03-23 20:56:22 +1100

    Added Menu

--- Helenote/app/src/main/java/com/example/spacegoing/helenote/NoteListFragment.java
+++ Helenote/app/src/main/java/com/example/spacegoing/helenote/NoteListFragment.java
@@ -4,8 +4,17 @@ package com.example.spacegoing.helenote;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 /**
@@ -18,12 +27,51 @@ public class NoteListFragment extends Fragment {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        ArrayAdapter<String> mForecastAdapter;
+
+        // Create some dummy data for the ListView.  Here's a sample weekly forecast
+        String[] data = {
+                "Mon 6/23 - Sunny - 31/17",
+                "Tue 6/24 - Foggy - 21/8",
+                "Wed 6/25 - Cloudy - 22/17",
+                "Thurs 6/26 - Rainy - 18/11",
+                "Fri 6/27 - Foggy - 21/10",
+                "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+                "Sun 6/29 - Sunny - 20/7"
+        };
+        List<String> weekForecast = new ArrayList<>(Arrays.asList(data));
+
+        mForecastAdapter = new ArrayAdapter<String>(getActivity(),
+                R.layout.note_item,
+                R.id.note_item_textview,
+                weekForecast);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_note_list, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_note_list, container, false);
+
+        ListView listView = (ListView) rootView.findViewById(R.id.notes_listView);
+        listView.setAdapter(mForecastAdapter);
+
+        return rootView;
     }
 
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.main_menu,menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
 }
