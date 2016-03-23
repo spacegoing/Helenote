176e56519e30b03a9f69ef9c79327a81734e84a3
spacegoing <spacegoing@gmail.com>
2016-03-23 17:57:31 +1100

    Start Writting Code

--- Helenote/app/src/main/java/com/example/spacegoing/helenote/MainActivity.java
+++ Helenote/app/src/main/java/com/example/spacegoing/helenote/MainActivity.java
@@ -9,5 +9,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // However, if we're being restored from a previous state,
+        // then we don't need to do anything and should return or else
+        // we could end up with overlapping fragments.
+        if (savedInstanceState != null) {
+            return;
+        }
+
+        // Create a new Fragment to be placed in the activity layout
+        NoteListFragment firstFragment = new NoteListFragment();
+
+        // In case this activity was started with special instructions from an
+        // Intent, pass the Intent's extras to the fragment as arguments
+        firstFragment.setArguments(getIntent().getExtras());
+
+        // Add the fragment to the 'fragment_container' FrameLayout
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.main_fragment_container, firstFragment).commit();
     }
+
+
+
 }
