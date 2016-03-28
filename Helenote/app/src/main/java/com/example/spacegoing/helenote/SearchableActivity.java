671eea9a1dd6eb20c1be10a5a052fb57eb25107f
spacegoing <spacegoing@gmail.com>
2016-03-28 18:10:15 +1100

    Start Adding Search View

---
+++ Helenote/app/src/main/java/com/example/spacegoing/helenote/SearchableActivity.java
@@ -0,0 +1,26 @@
+package com.example.spacegoing.helenote;
+
+import android.app.ListActivity;
+import android.app.SearchManager;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SearchableActivity extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_searchable);
+
+        // Get the intent, verify the action and get the query
+        Intent intent = getIntent();
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            doMySearch(query);
+        }
+    }
+
+    private void doMySearch(String query) {
+
+    }
+}
