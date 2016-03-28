52571b7115037a0c46cbaa059120f359f7e92bba
spacegoing <spacegoing@gmail.com>
2016-03-28 16:24:37 +1100

    Finish content-date-editDate

--- Helenote/app/src/main/java/com/example/spacegoing/helenote/NoteListAdapter.java
+++ Helenote/app/src/main/java/com/example/spacegoing/helenote/NoteListAdapter.java
@@ -22,25 +22,38 @@ public class NoteListAdapter extends CursorAdapter {
     public NoteListAdapter(Context context, Cursor c, int flags) {
         super(context, c, flags);
     }
+
     /*
         This is format the string to display
      */
-    private String convertCursorRowToUXFormat(Cursor cursor) {
-        String date = formatDate(cursor.getLong(NotesProvider.NOTE_COL_TIME_INDEX));
+    private void convertCursorRowToUXFormat(View view, Cursor cursor) {
+
+        TextView note_date_view = (TextView) view.findViewById(R.id.note_item_date);
+        TextView note_content_view = (TextView) view.findViewById(R.id.note_item_textview);
+        TextView note_label_view = (TextView) view.findViewById(R.id.note_item_label);
+
+        String dateTime = formatDateTime(cursor.getLong(NotesProvider.NOTE_COL_TIME_INDEX));
+        note_date_view.setText(dateTime);
+        String content = cursor.getString(NotesProvider.NOTE_COL_CONTENT_INDEX);
+        note_content_view.setText(content);
 
         String label;
-        if(cursor.getColumnIndex(NotesContract.RevisionEntry.COLUMN_EDITED_TIME)==-1){
+        String edited_time;
+        if (cursor.getColumnIndex(NotesContract.RevisionEntry.COLUMN_EDITED_TIME) == -1) {
             label = cursor.getString(NotesProvider.NOTE_COL_LABEL_INDEX);
-        }else{
-            label = cursor.getString(NotesProvider.REVISION_COL_EDITED_TIME_INDEX);
+            note_label_view.setText(label);
+        } else {
+            edited_time = "Last Edited Time: \n" + formatDateTime(
+                    cursor.getLong(NotesProvider.REVISION_COL_EDITED_TIME_INDEX)
+            );
+            note_content_view.setText(edited_time);
         }
 
-        return date + " - " + label;
     }
 
-    private static String formatDate(long dateInMillis) {
+    private static String formatDateTime(long dateInMillis) {
         Date date = new Date(dateInMillis);
-        return DateFormat.getDateInstance().format(date);
+        return DateFormat.getDateTimeInstance().format(date);
     }
 
     /*
@@ -58,7 +71,6 @@ public class NoteListAdapter extends CursorAdapter {
      */
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
-        TextView tv = (TextView)view;
-        tv.setText(convertCursorRowToUXFormat(cursor));
+        convertCursorRowToUXFormat(view, cursor);
     }
 }
