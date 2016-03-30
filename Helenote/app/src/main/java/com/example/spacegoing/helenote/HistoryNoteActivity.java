2cee498653066f6c1ad493be0256a57f063fa62f
spacegoing <spacegoing@gmail.com>
2016-03-30 13:12:45 +1100

    Added Authority

--- Helenote/app/src/main/java/com/example/spacegoing/helenote/HistoryNoteActivity.java
+++ Helenote/app/src/main/java/com/example/spacegoing/helenote/HistoryNoteActivity.java
@@ -17,7 +17,30 @@ import android.widget.ListView;
 
 import com.example.spacegoing.helenote.data.NotesContract;
 import com.example.spacegoing.helenote.data.NotesProvider;
-
+/**
+ * Created by spacegoing on 3/30/16.
+ *
+ * The code uses some snippets from Udacity Course *Developing Android Apps*.
+ * You can download the code through Github Link:
+ * https://github.com/udacity/Sunshine-Version-2
+ *
+ * However all code are with significant modifications under the following
+ * license:
+ *
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 public class HistoryNoteActivity extends AppCompatActivity {
 
     @Override
--- Helenote/app/src/main/java/com/example/spacegoing/helenote/MainActivity.java
+++ Helenote/app/src/main/java/com/example/spacegoing/helenote/MainActivity.java
@@ -1,8 +1,33 @@
+
 package com.example.spacegoing.helenote;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+/**
+ * Created by spacegoing on 3/30/16.
+ * <p>
+ * The code uses some snippets from Udacity Course *Developing Android Apps*.
+ * You can download the code through Github Link:
+ * https://github.com/udacity/Sunshine-Version-2
+ * <p>
+ * However all code are with significant modifications under the following
+ * license:
+ * <p>
+ * Copyright (C) 2014 The Android Open Source Project
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
